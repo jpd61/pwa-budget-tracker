36bee4b82f2328874c23b8b4ab4f0490eae4012b
jpd61 <jpd@dewoodyoil.com>
2020-07-26 10:00:06 -0500

    refactor

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,7 +13,7 @@ request.onupgradeneeded = function(event) {
 
 // upon a successful 
 request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
     // check if app is online, if yes run uploadTransaction() function to send all local db data to api
